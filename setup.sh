3f0b68e5a5e3db7ce9e0be0f2bf688541f319114
dar-ser <darina.komodo@gmail.com>
2026-06-12 15:26:43 +0200

    fixed gnuplot include

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "=== Installing system dependencies ==="
 sudo apt-get update
-sudo apt-get install -y build-essential cmake g++ libeigen3-dev gnuplot libqt5core5a libqt5gui5 libqt5widgets5
+sudo apt-get install -y build-essential cmake g++ libeigen3-dev gnuplot-nox
 
 echo "=== Initializing CAPD submodule ==="
 git submodule update --init --recursive
